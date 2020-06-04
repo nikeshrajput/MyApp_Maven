9cb2a9f9483d146ebf4a5a733b8a70581801c77a
Nikesh Singh <nikeshsingh78@gmail.com>
2020-06-04 22:02:34 +0200

    Adding ; at the EOL

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Editing one more line in this code")
+        System.out.println("Editing one more line in this code");
     }
 }
